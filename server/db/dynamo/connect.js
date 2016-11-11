86531f69559a739a01bb37db05547c35032558cb
Hunrik <henry.seres@gmail.com>
2016-11-11 17:33:04 +0100

    Fix

--- server/db/dynamo/connect.js
+++ server/db/dynamo/connect.js
@@ -1,11 +1,9 @@
 import fs from 'fs'
 import path from 'path'
-import dynamoose from 'dynamoose'
-import { dynamodb } from '../../config/secrets'
+/*import { dynamodb } from '../../config/secrets'*/
 
 export default () => {
   // Find the appropriate database to connect to, default to localhost if not found.
-  dynamoose.AWS.config.update(dynamodb)
   // Register schema as mongoose model
   const modelPath = path.join(__dirname, 'models')
   fs.readdirSync(modelPath).forEach((file) => {
