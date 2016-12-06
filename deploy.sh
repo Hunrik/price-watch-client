14b79c37859a9a7921c1beb1beaaa1f27fc96734
Hunrik <henry.seres@gmail.com>
2016-12-06 12:31:10 +0100

    Deploy fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ $DEPLOY_USER@D$DEPLOY_HOST:/var/www/deploy
 ssh $DEPLOY_USER@D$DEPLOY_HOST
 cd /var/www/deploy
-yarn
-cd public
-git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui
+yarn && npm run build
+cd public && git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui
+cd ../ && npm run stayAlive
