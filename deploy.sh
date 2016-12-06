04de4e94f840883023357a655c72010f7e48e60a
Hunrik <henry.seres@gmail.com>
2016-12-06 13:13:28 +0100

    Typo fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ $DEPLOY_USER@D$DEPLOY_HOST:/var/www/deploy || echo 'Deploy Failing'
 ssh $DEPLOY_USER@D$DEPLOY_HOST || echo 'SSH Failing'
 cd /var/www/deploy || echo 'CD Failing'
-yarn && npm run build || || echo 'Yarn Failing'
+yarn && npm run build || echo 'Yarn Failing'
 cd public && git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui || echo 'Github Failing'
 cd ../ && npm run stayAlive
