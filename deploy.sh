23bfb434c229fb1e43e3f9b93f9a58b2b31d07c5
Hunrik <henry.seres@gmail.com>
2016-12-06 13:04:26 +0100

    Logging deploy error

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ $DEPLOY_USER@D$DEPLOY_HOST:/var/www/deploy
-ssh $DEPLOY_USER@D$DEPLOY_HOST
-cd /var/www/deploy
-yarn && npm run build
-cd public && git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ $DEPLOY_USER@D$DEPLOY_HOST:/var/www/deploy || echo 'Deploy Failing'
+ssh $DEPLOY_USER@D$DEPLOY_HOST || echo 'SSH Failing'
+cd /var/www/deploy || echo 'CD Failing'
+yarn && npm run build || || echo 'Yarn Failing'
+cd public && git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui || echo 'Github Failing'
 cd ../ && npm run stayAlive
