1e36a38cf0e256757f7f782d585183b793da7a73
Hunrik <henry.seres@gmail.com>
2016-12-06 19:53:49 +0100

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-tar -xzf package.tgz && rm package.tgz
+git pull git@github.com:Hunrik/price-watch-client.git
+cd public && git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui
 yarn && npm run build
 cd public && git clone git@github.com:Semantic-Org/Semantic-UI-CSS.git semantic-ui
 cd ../ && npm run stayAlive
